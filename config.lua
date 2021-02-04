391948d22ad6590a754dd6e8bb5acccea6ea9feb
Ali <74619504+Utinax@users.noreply.github.com>
2021-02-04 22:28:41 +0300

    Emlakçı seçeneği eklendi.

--- config.lua
+++ config.lua
@@ -1,5 +1,6 @@
 Config = {}
 
+Config.Emlak = false -- true yaparsanız yalnızca emlak olanlar para aklayabilir.
 Config.Koordinat = {x = -867.019, y = -1275.26, z = 5.1501} -- Kara paranın yıkanacağı Koordinat
 Config.GerekliSayi = 100 -- Kara para yıkamak için gereken minimum kara para miktarı
 Config.Bolum = 2 -- Paranın kaça bölüneceği. Örn: 2 yaparsanız üzerinizde 100 kara para varsa 50 nakit para olarak size döner. 1 yaparsanız kara para miktarına eşit olarak nakit para verir.
